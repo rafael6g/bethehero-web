86dcc5411c9a0edafc7d7bf5bce127712787ddac
Rafael C Fernandes <rafael.londrina@gmail.com>
2020-11-11 09:12:57 -0300

    first commit

---
+++ build/precache-manifest.76e3e51c734e83f7344342a72b156615.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cea40053116ecd85c57cdf411f2e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f4595aa5434d1f7c89",
+    "url": "/static/css/main.f1d0eaf1.chunk.css"
+  },
+  {
+    "revision": "40a61d9fe70c896b896e",
+    "url": "/static/js/2.328d2148.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.328d2148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f4595aa5434d1f7c89",
+    "url": "/static/js/main.5712ecf7.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
